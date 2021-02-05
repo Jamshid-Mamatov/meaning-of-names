4ef0065f70e38468dde7d0de89c4ddce0f9bb4dc
Jamshid <mamatovjamshid506@gmail.com>
2021-02-05 11:30:47 +0500

    while is code working regularly

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ def update():
     update_id=data[-1]['update_id']
     chat_id=data[-1]['message']['chat']['id']
     text=data[-1]['text']
+    inform=[update_id,chat_id,text]
+    return inform
+
+
+last_update_id=None
+while True:
+    inform=update()
+    update_id=inform[0]
+    if last_update_id!=update_id:
+        last_update_id=update_id
