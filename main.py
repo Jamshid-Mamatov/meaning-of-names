0088fbbcfd7eedb716e1a9aa7e7189178be49644
Jamshid <mamatovjamshid506@gmail.com>
2021-02-05 22:21:16 +0500

    update id

--- main.py
+++ main.py
@@ -11,18 +11,26 @@ def update():
 
     r=requests.get(url)
     data=r.json()['result']
-    update_id=data[-1]['update_id']
+    
     chat_id=data[-1]['message']['chat']['id']
     text=data[-1]['message']['text']
-    inform=[update_id,chat_id,text]
+    inform=[chat_id,text]
     return inform
+
+def update_id():
+    url=f'https://api.telegram.org/bot{token}/getUpdates'
+
+    r=requests.get(url)
+    data=r.json()['result']
+    update_id=data[-1]['update_id']
+    return update_id
+
 
 def get_name_mean(name):
     url=f'https://ismlar.com/name/{name}'
     r=requests.get(url)
     
-    
-        
+       
     data=str(r.text)
     response=data.find(name)
     if response!=-1:
@@ -41,9 +49,9 @@ def get_name_mean(name):
 
 def sendMsg():
     url=f'https://api.telegram.org/bot{token}/sendMessage'
-    infrom=update()
-    text=get_name_mean(inform[2])
-    chat_id=inform[1]
+    inform=update()
+    text=get_name_mean(inform[1])
+    chat_id=inform[0]
     payload={
         'chat_id':chat_id,
         'text':text
@@ -54,11 +62,10 @@ def sendMsg():
 
 last_update_id=None
 while True:
-    inform=update()
-    update_id=inform[0]
+    new_update_id=update_id()
     
-    if last_update_id!=update_id:
-        last_update_id=update_id
+    if last_update_id!=new_update_id:
+        last_update_id=new_update_id
         
         sendMsg()
 
